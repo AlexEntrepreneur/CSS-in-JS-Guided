f112a0192404b6ad999dccbb3abafe9985c92578
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-01-23 11:03:29 +0000

    .

--- src/Reactstrap.js
+++ src/Reactstrap.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import { Button, Spinner, Input } from 'reactstrap';
 
 export default function Reactstrap() {
   return (
-    <div className="reactstap-container">
+    <div className="reactstap-container" style={{textAlign: 'center'}}>
       <Button>Hello From Reactstrap!</Button>
       <Button color="primary">Hello From Reactstrap!</Button>
+      <Spinner type="grow" color="primary" />
+      <Spinner color="danger" />
+      <Input type="select" name="select" id="exampleSelect">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+      </Input>
+      <Spinner size="sm" color="success" style={{ width: '6rem', height: '10rem' }} />
     </div>
   );
 }
