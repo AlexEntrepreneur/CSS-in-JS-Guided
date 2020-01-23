5380b6427f22beff71a96f76b938db96da494e11
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-01-23 10:11:30 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from 'styled-components';
 
 function App() {
   return (
@@ -9,8 +10,26 @@ function App() {
         </span>{" "}
         Advanced Styling Techniques
       </h1>
+      <Button>Some Text <span>Spanned</span></Button>
     </div>
   );
 }
+
+const Button = styled.button`
+  background: dodgerblue;
+  color: white;
+  font-size: 20px;
+  padding: 10px;
+  min-width: 150px;
+
+  &:hover {
+    transform: scale(1.2);
+  }
+
+  & span {
+    color: red;
+    font-weight: bold;
+  }
+`;
 
 export default App;
