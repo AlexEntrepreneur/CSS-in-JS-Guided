b2125844aa34f70f9275d8e3cb2ca54f4fc902a7
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-01-23 10:54:22 +0000

    .

--- src/Reactstrap.js
+++ src/Reactstrap.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from 'reactstrap';
 
 export default function Reactstrap() {
   return (
     <div className="reactstap-container">
-
+      <Button>Hello From Reactstrap!</Button>
+      <Button color="primary">Hello From Reactstrap!</Button>
     </div>
   );
 }
